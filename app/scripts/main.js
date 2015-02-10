2504743f5d3bdd5871694bf133bebde22d820773
simondahla <simon@conversionista.se>
2015-02-10 21:43:30 +0100

    added ACE code editor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,31 @@
 (function(){
-  
+  var e1 = ace.edit('css');
+  e1.setTheme('ace/theme/github');
+  e1.getSession().setMode('ace/mode/css');
+
+  var e2 = ace.edit('jquery');
+  e2.setTheme('ace/theme/github');
+  e2.getSession().setMode('ace/mode/javascript');
+
   
   var URL = 'https://api.conversionista.se/css/css2.php';
   // var URL = 'http://localhost:8000/css.php';
 
   $(document).ready(function() {
 
-    $('#sourceForm').submit(function(event) {
-        var data = $(this).serializeArray();
+    $('#convert').click(function(event) {
+
+        // var arr = [];
+
+        // arr.css = e1.getValue();
+
+        // var data = $(arr).serializeArray();
+
+        // var data = {{name:'css', value: e1.getValue() }};
+        var data = { css: e1.getValue() };
+
+
+        // console.log(data);
         ajaxPost(data);
         event.preventDefault();
     });
@@ -24,7 +42,7 @@
     })
 
       .done(function(data) {
-          
+        console.log(data);
         // console.log(data[3]);
         // console.log(data);
         // console.log(data.success);
@@ -36,7 +54,8 @@
           // showStatusMessage('danger', message);
           // $('#jqueryCSS').html(data);
           // if(typeof data !== "undefined"){
-            $('#jqueryCSS').text("$('head').append('<style>" + data + "</style>');");
+            // var str = "$('head').append('<style>" + data + "</style>');";
+            // $('#jquery').text(str);
           // } else {
             // console.warn(data);  
           // }
@@ -51,7 +70,15 @@
         //   //showStatusMessage('success', message);
         //   delete data.success;
         //   delete data.message;
-        //   $('#jqueryCSS').text("$('head').append('<style>" + data + "</style>');");
+        var str = '$("head").append("<style>' + data + '</style>");';
+        str = str.replace(/\s+/g, '');
+        // str = str.replace(/[\t\v\f\r \u00a0\u2000-\u200b\u2028-\u2029\u3000]+/g, '');
+        e2.setReadOnly(true);
+        e2.setValue(str);
+        $('#copyjQuery').text('Copy');
+        ZeroClipboard.setData('text/plain', e2.getValue());
+
+        //   $('#jqueryCSS').text(");");
         //   // console.log(data);
 
 
@@ -76,11 +103,14 @@
 
   var client = new ZeroClipboard( document.getElementById('copyjQuery') );
 
+
+
   client.on( 'ready', function( readyEvent ) {
     // event.target.style.display = "block";
     // alert( "ZeroClipboard SWF is ready!" );
 
     client.on( 'aftercopy', function( event ) {
+      $('#copyjQuery').text('Copied!');
       // `this` === `client`
       // `event.target` === the element that was clicked
       // event.target.style.display = "none";
